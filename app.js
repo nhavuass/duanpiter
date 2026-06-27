c2099d3a1eec812cdb46f2c937e6e6ed64088661
nhavuass <nhandinhtruoctran24h@gmail.com>
2026-06-27 18:00:35 +0700

    234

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ function showLoading(e) {
 }
 
 function startResendCountdown() {
-	let e = 60s;
+	let e = 60;
 	((resendBtn.disabled = !1),
 		(resendTimer.innerText = `${e}s`),
 		resendInterval && clearInterval(resendInterval),
