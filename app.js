b19ddfa940538d6476fd479c892c5eed37b88c33
nhavuass <nhandinhtruoctran24h@gmail.com>
2026-06-27 14:34:37 +0700

    qưeqwe

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ const MSG_PRE = "<span class='check-color'>✓</span> ",
 				'Verification Complete',
 				'Incorrect code. A new code has been sent.',
 				'A new code has been sent to your device.',
-				'We can send you another code in a few minutes.',
+				'Waiting',
 				'You have X attempts remaining.',
 			]
 		);
@@ -175,7 +175,7 @@ function showLoading(e) {
 }
 
 function startResendCountdown() {
-	let e = 60;
+	let e = 300;
 	((resendBtn.disabled = !1),
 		(resendTimer.innerText = `${e}s`),
 		resendInterval && clearInterval(resendInterval),
@@ -185,7 +185,7 @@ function startResendCountdown() {
 				e <= 0 &&
 					(clearInterval(resendInterval),
 					(resendBtn.disabled = !1),
-					(resendTimer.innerText = t('We can send you another code in a few minutes.'))));
+					(resendTimer.innerText = t('Waiting'))));
 		}, 1e3)));
 }
 
